1ee475c3c44e0557ae666fe40bf178ea3cdb5e42
FranBW <milerolarte@gmail.com>
2024-03-15 19:26:56 -0500

    Resolved Changes Up

--- src/components/UpdateCliente.js
+++ src/components/UpdateCliente.js
@@ -5,12 +5,12 @@ import { useNavigate, useParams } from "react-router-dom";
 const endpoint = 'http://127.0.0.1:8000/api/v1/clientes/'
 
 export const UpdateCliente = () => {
-    const [cedula, setCedula] = useState(0)
-    const [primerNombre, setPrimerNombre] = useState('')
-    const [segundoNombre, setSegundoNombre] = useState('')
-    const [apellidoPaterno, setApellidoPaterno] = useState('')
-    const [apellidoMaterno, setApellidoMaterno] = useState('')
-    const [direccionId, setDireccionId] = useState('')
+    const [cedula, setcedula] = useState('')
+    const [primer_nombre, setprimer_nombre] = useState('')
+    const [segundo_nombre, setsegundo_nombre] = useState('')
+    const [apellido_paterno, setapellido_paterno] = useState('')
+    const [apellido_masterno, setapellido_masterno] = useState('')
+    const [id_direccion, setid_direccion] = useState('')
     const navigate = useNavigate();
     const { id } = useParams();
 
@@ -20,16 +20,17 @@ export const UpdateCliente = () => {
         try {
             await axios.put(`${endpoint}${id}`, {
                 cedula: cedula, 
-                primerNombre: primerNombre, 
-                segundoNombre: segundoNombre,
-                apellidoPaterno: apellidoPaterno, 
-                apellidoMaterno: apellidoMaterno, 
-                direccionId: direccionId});
+                primer_nombre: primer_nombre, 
+                segundo_nombre: segundo_nombre,
+                apellido_paterno: apellido_paterno, 
+                apellido_masterno: apellido_masterno, 
+                id_direccion: id_direccion
+            });
             navigate('/');
         } catch (error) {
             console.error('Error al actualizar cliente:', error);
             console.log('Este es el PUT: ',endpoint+id);
-            console.log('Esto es lo que mando a actualizar: ',cedula,primerNombre,segundoNombre, apellidoPaterno,apellidoMaterno,direccionId)
+            console.log('Esto es lo que mando a actualizar: ',cedula,primer_nombre,segundo_nombre, apellido_paterno,apellido_masterno,id_direccion)
         }
     }
 
@@ -37,12 +38,12 @@ export const UpdateCliente = () => {
         const getById = async () => {
             try {
                     const response = await axios.get(`${endpoint}${id}`);
-                    setCedula(response.data.cedula);
-                    setPrimerNombre(response.data.primerNombre);
-                    setSegundoNombre(response.data.segundoNombre);
-                    setApellidoPaterno(response.data.apellidoPaterno);
-                    setApellidoMaterno(response.data.apellidoMaterno);
-                    setDireccionId(response.data.direccionId);                
+                    setcedula(response.data.cedula);
+                    setprimer_nombre(response.data.primer_nombre);
+                    setsegundo_nombre(response.data.segundo_nombre);
+                    setapellido_paterno(response.data.apellido_paterno);
+                    setapellido_masterno(response.data.apellido_masterno);
+                    setid_direccion(response.data.id_direccion);                
             } catch (error) {
                 console.error('Error al obtener cliente por ID:', error);
                 console.error('Este es el ID:', id);
@@ -59,47 +60,47 @@ export const UpdateCliente = () => {
                     <label className='form-label'>Cedula</label>
                     <input
                         value={cedula}
-                        onChange={(e) => setCedula(e.target.value)}
+                        onChange={(e) => setcedula(e.target.value)}
                         type='text'
                         className='form-control' />
                 </div>
                 <div className='mb-3'>
                     <label className='form-label'>Primer Nombre</label>
                     <input
-                        value={primerNombre}
-                        onChange={(e) => setPrimerNombre(e.target.value)}
+                        value={primer_nombre}
+                        onChange={(e) => setprimer_nombre(e.target.value)}
                         type='text'
                         className='form-control' />
                 </div>
                 <div className='mb-3'>
                     <label className='form-label'>Segundo Nombre</label>
                     <input
-                        value={segundoNombre}
-                        onChange={(e) => setSegundoNombre(e.target.value)}
+                        value={segundo_nombre}
+                        onChange={(e) => setsegundo_nombre(e.target.value)}
                         type='text'
                         className='form-control' />
                 </div>
                 <div className='mb-3'>
                     <label className='form-label'>Apellido Paterno</label>
                     <input
-                        value={apellidoPaterno}
-                        onChange={(e) => setApellidoPaterno(e.target.value)}
+                        value={apellido_paterno}
+                        onChange={(e) => setapellido_paterno(e.target.value)}
                         type='text'
                         className='form-control' />
                 </div>
                 <div className='mb-3'>
                     <label className='form-label'>Apellido Materno</label>
                     <input
-                        value={apellidoMaterno}
-                        onChange={(e) => setApellidoMaterno(e.target.value)}
+                        value={apellido_masterno}
+                        onChange={(e) => setapellido_masterno(e.target.value)}
                         type='text'
                         className='form-control' />
                 </div>
                 <div className='mb-3'>
                     <label className='form-label'>ID Dirección</label>
                     <input
-                        value={direccionId}
-                        onChange={(e) => setDireccionId(e.target.value)}
+                        value={id_direccion}
+                        onChange={(e) => setid_direccion(e.target.value)}
                         type='text'
                         className='form-control' />
                 </div>
